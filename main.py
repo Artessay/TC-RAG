cad60c68291d857f13f66ebf6882c37d1fd15949
YZZBBB <teacher3706@sina.com>
2024-12-17 14:01:35 +0800

    hello git

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from utils import get_args
 from analysis import evaluate
 from dotenv import load_dotenv
 from extract_by_qwen import extract_answer
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,5,6,7"
 
 def main():
     load_dotenv()
